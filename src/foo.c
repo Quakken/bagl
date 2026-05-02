128d3bc92ee50077fb98447c40172d8658be8edd
Quakken <69021181+Quakken@users.noreply.github.com>
2026-05-02 15:38:31 -0700

    Imported GLAD
    Added GLAD target, linked against main library

--- src/foo.c
+++ src/foo.c
@@ -1,9 +1,12 @@
 #include "foo.h"
+
+#include "glad/glad.h"
 #include "GLFW/glfw3.h"
 
 #include "internal/stb_image.h"
 
 int doThings() {
+  gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
   glfwInit();
 
   uint8_t *imageData = stbi_load("asset.png", NULL, NULL, NULL, 0);
