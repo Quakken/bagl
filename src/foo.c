3f42a6a6ea1c0aa247ec5dc85bac7133de5d567c
Quakken <69021181+Quakken@users.noreply.github.com>
2026-05-02 15:23:45 -0700

    Imported stb_image
    Added stb_image.h to source

--- src/foo.c
+++ src/foo.c
@@ -1,7 +1,11 @@
 #include "foo.h"
 #include "GLFW/glfw3.h"
 
+#include "internal/stb_image.h"
+
 int doThings() {
   glfwInit();
+
+  uint8_t *imageData = stbi_load("asset.png", NULL, NULL, NULL, 0);
   return 0;
 }
